d4da175872c194d40225859d9af06b3c6317d778
Daniel Wang <danielwang655@31-38-108.wireless.csail.mit.edu>
2022-01-10 21:11:35 -0500

    Changed database strings.

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "mongodb+srv://mna144:mongodb2022@cluster0.8vveo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoConnectionURL = "mongodb+srv://danielwsunny655:Fulong91@cluster0.it7f8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
 const databaseName = "myFirstDatabase";
 
