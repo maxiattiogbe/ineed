9f231f9e2f7ce2416ab8553e1b68541444b288ea
mna1024 <maxiattiogbe@gmail.com>
2022-01-14 13:58:24 -0500

    Centering Google login button and changing home cursor color

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -8,25 +8,27 @@ const GOOGLE_CLIENT_ID = "942346034916-8oq8cetdofgeupecljks6lgmgabq083l.apps.goo
 const Login = ({ userId, handleLogin, handleLogout }) => {
   return (
     <>
-      {userId ? (
-        <GoogleLogout
-          clientId={GOOGLE_CLIENT_ID}
-          buttonText="Logout"
-          onLogoutSuccess={handleLogout}
-          onFailure={(err) => console.log(err)}
-        />
-      ) : (
-        <GoogleLogin
-          clientId={GOOGLE_CLIENT_ID}
-          buttonText="Login"
-          onSuccess={handleLogin}
-          onFailure={(err) => console.log(err)}
-        />
-      )}
         <br></br>
         <div className="body-title-2 u-inlineBlock u-textCenter">Login</div>
-  
-      
+        <br></br>
+        <br></br>
+        <div className="u-textCenter">
+            {userId ? (
+            <GoogleLogout
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Logout"
+            onLogoutSuccess={handleLogout}
+            onFailure={(err) => console.log(err)}
+            />
+        ) : (
+            <GoogleLogin
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={handleLogin}
+            onFailure={(err) => console.log(err)}
+            />
+        )}
+        </div>
       
     </>
   );
