bb5f321019bd1433607810ab4c9f6264def528dc
BBunch19 <thbunches@gmail.com>
2022-01-14 22:56:22 -0500

    feed page, feed css

--- client/src/components/pages/Feed.js
+++ client/src/components/pages/Feed.js
@@ -1,11 +1,61 @@
 import React, { Component } from "react";
 import "../../utilities.css";
+import "./Feed.css"
 
 const Feed = () => {
     return (
         <>
         <br></br>
         <div className="body-title-2 u-textCenter">Feed</div>
+        
+        <div className="boxed">
+            < img className = "profilepic" src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt = "profile pic" ></img>
+           <div className = "name"> Name</div> 
+           <br></br>
+            <div className = "ineed">I Need</div> 
+            <div className = "postInfo">Info</div>
+            <br></br>
+            <div className = "ioffer">I offer</div>
+            <div className = "postInfo">Info</div>
+            <br></br>
+            <div classname = "other">Other</div>
+            <div className = "postInfo">Info</div>
+            <br></br>
+        </div>
+
+        <div className="boxed">
+        < img className = "profilepic" src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt = "profile pic" ></img>
+           <div className = "name"> Name</div> 
+           <br></br>
+            <div className = "ineed">I Need</div> 
+            <div className = "postInfo">Info</div>
+            <br></br>
+            <div className = "ioffer">I offer</div>
+            <div className = "postInfo">Info</div>
+            <br></br>
+            <div classname = "other">Other</div>
+            <div className = "postInfo">Info</div>
+            <br></br>
+        </div>
+
+        <div className="boxed">
+        < img className = "profilepic" src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt = "profile pic" ></img>
+           <div className = "name"> Name</div> 
+           <br></br>
+            <div className = "ineed">I Need</div> 
+            <div className = "postInfo">Info</div>
+            <br></br>
+            <div className = "ioffer">I offer</div>
+            <div className = "postInfo">Info</div>
+            <br></br>
+            <div classname = "other">Other</div>
+            <div className = "postInfo">Info</div>
+            <br></br>
+        </div>
+
+
+
+
         </>
     );
 }
