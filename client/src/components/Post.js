8c8d59008083aba8b203816fa6e66279ffb10929
BBunch19 <thbunches@gmail.com>
2022-01-15 16:05:40 -0500

    changed the feed page and css

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -4,7 +4,7 @@ import "../utilities.css";
 
 const Post = ({name,ineed,offer,other}) => {
     return (
-        
+      
         <div className="boxed">
         < img className = "profilepic" src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt = "profile pic" ></img>
        <div className = "name">{name}</div> 
@@ -15,7 +15,7 @@ const Post = ({name,ineed,offer,other}) => {
         <div className = "ioffer">I offer</div>
         <div className = "postInfoOffer">{offer}</div>
         <br></br>
-        <div classname = "other">Other</div>
+        <div className = "other" >Other</div>
         <div className = "postInfoOther">{other}</div>
         <br></br>
           </div>
