a6e3c7f0ae345a63bc87f8cf7ef91a331dfe507a
mna1024 <maxiattiogbe@gmail.com>
2022-01-19 21:20:04 -0500

    Cannot access features when logged out

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -54,9 +54,15 @@ const App = () => {
         <Router>
           <Home path="/" />
           <Login path="/login" handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />
-          <Profile path="/profile" name={userName} userId={userId} />
-          <Feed path="/feed" />
-          <NewPost path="/new-post" name={userName}/>
+          {userId && (
+            <Profile path="/profile" name={userName} userId={userId} />
+          )}
+          {userId && (
+            <Feed path="/feed" />
+          )}
+          {userId && (
+            <NewPost path="/new-post" name={userName}/>
+          )}
           <NotFound default />
         </Router>
       </div>
