9135a356d35d1125b662087f0e7560dfb70f2997
mna1024 <maxiattiogbe@gmail.com>
2022-01-21 00:52:56 -0500

    Reponsive font size

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -64,15 +64,6 @@ const Home = () => {
   };
   return (
     <>
-      {/* <img src="https://images.pexels.com/photos/6146951/pexels-photo-6146951.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" className="Home-img2" /> */}
-      {/* <div className="Home-img">
-        <div class="Home-overlay">
-          <div className="Home-body-title-4 u-inlineBlock u-textCenter">
-            The social way to get what you need.
-          </div>
-        </div>
-      </div> */}
-     
       <div className="Home-img">
         <div className="Home-body-title-4 u-inlineBlock u-textCenter">
           <br></br>
