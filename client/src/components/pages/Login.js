1643e9fad52c099089774f395b90f5dc628caa89
mna1024 <maxiattiogbe@gmail.com>
2022-01-23 21:30:10 -0500

    Login page links

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -3,6 +3,7 @@ import { Link } from "@reach/router";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
 
 import "../../utilities.css";
+import "./Login.css";
 
 const GOOGLE_CLIENT_ID = "942346034916-b4mth5m9bhtvlnpuppa4djfu0olrdj0i.apps.googleusercontent.com";
 
@@ -34,10 +35,20 @@ const Login = ({ userId, handleLogin, handleLogout, name}) => {
             {userId && (<div className="body-text-1">Welcome {name}!</div>)}
             {userId && (<br></br>)}
             {userId && (<br></br>)}
-            {userId && (<div className="body-text-1">Check out the Home page if it's your first time here or you just need a
+            {userId && (<div className="body-text-1">Check out the <Link to={`/`} className="Login-body-link">
+               Home
+             </Link> page if it's your first time here or you just need a
              refresher on how to use Ineed.</div>)}
-             {userId && (<div className="body-text-1">Otherwise, head straight to New Post to make a post and get what you need!</div>)}
-             {userId && (<div className="body-text-1">Don't forget to check for anything new in Feed and Messages.</div>)}
+             {userId && (<div className="body-text-1">Otherwise, head straight to <Link to={`/new-post/`} className="Login-body-link">
+            New Post
+          </Link> to make a post and get what you need!</div>)}
+             {userId && (<div className="body-text-1">Made a mistake? No worries! Go to <Link to={`/profile/`} className="Login-body-link">
+            Profile
+          </Link> to edit or delete posts.</div>)}
+             {userId && (<div className="body-text-1">Don't forget to check <Link to={`/feed/`} className="Login-body-link">
+            Feed
+          </Link> for any new posts</div>)}
+          {userId && (<div className="body-text-1"> and <Link to={`/messages/`} className="Login-body-link">Messages</Link> for new messages from fellow users.</div>)}
           </div>
     </>
   );
